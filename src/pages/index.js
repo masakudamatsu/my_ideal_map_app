197ee14fae1839933fd393341dad304db535d850
Masa Kudamatsu <masakudamatsu@gmail.com>
2021-06-08 15:46:40 +0900

    build(styled-components): set up Styled Components
    use CSS-in-JS
    
    close #6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,15 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`;
+
 function HomePage() {
-  return <div>Welcome to Next.js!</div>;
+  return <Button>Welcome to Next.js!</Button>;
 }
 export default HomePage;
